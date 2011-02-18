4fde48b2bf434775def92f32443a3ddd88108417
Brett Buddin <brett@intraspirit.net>
2011-02-18 15:53:12 -0500

    Back to hyphens.

--- lib/labor/helpers.rb
+++ lib/labor/helpers.rb
@@ -1,7 +1,7 @@
 module Labor
   module Helpers
     def classify(word)
-      word.split(/-_/).each { |part| part[0] = part[0].chr.upcase }.join
+      word.split('-').each { |part| part[0] = part[0].chr.upcase }.join
     end
 
     def constantize(camel_cased_word)

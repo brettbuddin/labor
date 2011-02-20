797399b568c8cc1cc2d9210891a4695a1cff318b
Brett Buddin <brett@intraspirit.net>
2011-02-20 18:20:43 -0500

    Config loading inteface tests

--- spec/labor_spec.rb
+++ spec/labor_spec.rb
@@ -12,4 +12,22 @@ describe Labor do
       Labor.servers.should == ["10.0.0.1:4370", "10.0.0.2:4370"]
     end
   end
+  
+  describe :configuration do
+    before(:each) do
+      Labor.config = File.join(File.dirname(__FILE__), 'files', 'sample_config.rb')
+    end
+
+    after(:each) do
+      Labor.instance_variable_set(:@config, nil)
+    end
+
+    it "can instantiate the config object" do
+      Labor.config.should be_instance_of(Labor::Config)
+    end
+
+    it "can retreive a key from the loaded config file" do
+      Labor.config.foo.should == "bar"
+    end
+  end
 end

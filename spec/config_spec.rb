f674afe66c9fa86215bb21039e33261b84ccfe5a
Brett Buddin <brett@intraspirit.net>
2011-02-20 20:00:17 -0500

    More descriptive spec for config

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -5,59 +5,65 @@ describe Labor::Config do
     @config = Labor::Config.new
   end
 
-  it "loads config from a file" do
-    @config.load File.join(File.dirname(__FILE__), 'files', 'sample_config.rb')
-    @config.get(:foo).should == "bar"
+  describe "config files" do
+    it "can be loaded" do
+      @config.load File.join(File.dirname(__FILE__), 'files', 'sample_config.rb')
+      @config.get(:foo).should == "bar"
+    end
+
+    it "should provide in-line variables for set keys" do
+      @config.instance_eval <<-CONFIG
+        set :foo, "bar"
+        set :i_am, "at the \#{foo}"
+      CONFIG
+      @config.get(:i_am).should == "at the bar"
+    end
   end
 
-  it "allows write access to a key" do
-    @config.set(:foo, "bar")
-    @config.get(:foo).should == "bar"
+  describe "write access" do
+    it "provides access to a key" do
+      @config.set(:foo, "bar")
+      @config.get(:foo).should == "bar"
+    end
+
+    it "provides access to keys via bracket method" do
+      @config[:foo] = "bar"
+      @config.get(:foo).should == "bar"
+    end
+
+    it "allows deletion of a key" do
+      @config.set(:foo, "a noob")
+      @config.delete(:foo)
+      @config.get(:foo).should be_nil
+    end
   end
 
-  it "allows read access to a key" do
-    @config.set(:foo, "bar")
-    @config.get(:foo).should == "bar"
-  end
+  describe "read access" do
+    it "provides access to a key" do
+      @config.set(:foo, "bar")
+      @config.get(:foo).should == "bar"
+    end
 
-  it "allows read access to keys via bracket method" do
-    @config.set(:foo, "bar")
-    @config[:foo].should == "bar"
-  end
+    it "provides access to keys via bracket method" do
+      @config.set(:foo, "bar")
+      @config[:foo].should == "bar"
+    end
 
-  it "provides access to keys using methods" do
-    @config.set(:foo, "bar")
-    @config.foo.should == "bar"
-  end
+    it "provides access to keys using methods" do
+      @config.set(:foo, "bar")
+      @config.foo.should == "bar"
+    end
 
-  it "allows write access to keys via bracket method" do
-    @config[:foo] = "bar"
-    @config.get(:foo).should == "bar"
-  end
-
-  it "allows deletion of a key" do
-    @config.set(:foo, "a noob")
-    @config.delete(:foo)
-    @config.get(:foo).should be_nil
-  end
-
-  it "allows checking existence of a key" do
-    @config.set(:foo, "bar")
-    @config.exists?(:foo)
+    it "allows checking existence of a key" do
+      @config.set(:foo, "bar")
+      @config.exists?(:foo)
+    end
   end
 
   it "have each defined" do
     @config.respond_to?(:each)
   end
-
-  it "allow configs to use keys as in-line variables" do
-    @config.instance_eval <<-CONFIG
-      set :foo, "bar"
-      set :i_am, "at the \#{foo}"
-    CONFIG
-    @config.get(:i_am).should == "at the bar"
-  end
-
+  
   describe :grouping do
     before(:each) do
       @config.instance_eval <<-CONFIG
